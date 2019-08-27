7efbb525ef117cad87f96bfa77f2d4a9e73541ac
Kierran McPherson <kierranm@gmail.com>
2019-08-27 17:44:02 +1200

    Basic build chain

---
+++ .teamcity/settings.kts
@@ -0,0 +1,48 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+
+version = "2019.1"
+
+project {
+    sequence {
+        build(BuildA)
+        build(BuildB)
+        build(BuildC)
+    }
+}
+
+object BuildA : BuildType({
+    name = "A"
+    steps {
+        script {
+            name = "Hello World"
+            scriptContent = """
+                echo "hello world! I'm a build"
+            """.trimIndent()
+        }
+    }
+})
+
+object BuildB : BuildType({
+    name = "B"
+    steps {
+        script {
+            name = "Hello World"
+            scriptContent = """
+                echo "hello world! I'm a build"
+            """.trimIndent()
+        }
+    }
+})
+
+object BuildC : BuildType({
+    name = "C"
+    steps {
+        script {
+            name = "Hello World"
+            scriptContent = """
+                echo "hello world! I'm a build"
+            """.trimIndent()
+        }
+    }
+})
